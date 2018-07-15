8c79914e07dbd1459fbcb47f27ea7cd612202551
hrishikeshd10 <hrishi.montfort@gmail.com>
2018-07-15 09:47:40 +0530

    final commit----the login system is ready

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,29 +1,37 @@
-import 'package:firebase_auth/firebase_auth.dart' show FirebaseAuth, FirebaseUser;
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 
-abstract class BaseAuth{
-  Future<String> SignInWithEmailAndPassword(String email, String password);
-  Future <String> CreateUserWithEmailAndPassword(String email, String password);
-
-
+abstract class BaseAuth {
+  Future<String> signInWithEmailAndPassword(String email, String password);
+  Future<String> createUserWithEmailAndPassword(String email, String password);
+  Future<String> currentUser();
+  Future<void> signOut();
 }
 
-class Auth implements BaseAuth{
+class Auth implements BaseAuth {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-//**********make sure to add the method prototypes to the "abstracr class BaseAuth"************* */
-
-
-  Future<String> SignInWithEmailAndPassword(String email, String password) async {
-
-     FirebaseUser user = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
-      return user.uid;
+  Future<String> signInWithEmailAndPassword(
+      String email, String password) async {
+    FirebaseUser user = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+    user.email;
+    return user?.uid;
   }
 
-Future <String> CreateUserWithEmailAndPassword(String email, String password) async{
+  Future<String> createUserWithEmailAndPassword(
+      String email, String password) async {
+    FirebaseUser user = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    return user?.uid;
+  }
 
-  FirebaseUser user = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email,password: password);
-  return user.uid;
+  Future<String> currentUser() async {
+    FirebaseUser user = await _firebaseAuth.currentUser();
+    return user?.uid;
+  }
 
-}
-
+  Future<void> signOut() async {
+    return _firebaseAuth.signOut();
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
-import 'LoginPage.dart';
-import "auth.dart";
-void main()
-{
-   
-   runApp(new MyApp());
-}
+import 'package:login_demo/auth.dart';
+import 'package:login_demo/root_page.dart';
 
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Login Demo',
-      theme: new ThemeData(primarySwatch: Colors.lightGreen),
-      home: new LoginPage(auth: new Auth(),),
-      debugShowCheckedModeBanner: false,
-      
-    );
+    return MaterialApp(
+        title: 'Flutter login demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: RootPage(auth: Auth()));
   }
 }
